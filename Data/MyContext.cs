df8279a4cecae70168bfc77b3d30b4b54a810432
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-02-06 06:22:35 +0200

    Kill me...

--- Data/MyContext.cs
+++ Data/MyContext.cs
@@ -5,19 +5,55 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using daw.Models;
 
-namespace daw.Data
-{
-    public class MyContext: DbContext
-    {
-        public MyContext(DbContextOptions<MyContext> options) : base(options)
-        {
+namespace daw.Data {
+    public class MyContext: DbContext {
+        public MyContext(DbContextOptions < MyContext > options): base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.Entity < Cast > ().HasKey(table => new {
+                table.movieId, table.actorId
+            });
+
+            builder.Entity < MovieAward > ().HasKey(table => new {
+                table.awardId, table.movieId
+            });
+
+            builder.Entity < MovieLanguage > ().HasKey(table => new {
+                table.movieId, table.languageId
+            });
+
+            builder.Entity < MovieGenre > ().HasKey(table => new {
+                table.genreId, table.movieId
+            });
+
+            builder.Entity < Watchlist > ().HasKey(table => new {
+                table.userId, table.movieId
+            });
         }
 
-        public DbSet<Actor> Actors { get; set; }
-        public DbSet<Award> Awards { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<Language> Languages { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet < Actor > Actors {
+            get;
+            set;
+        }
+        public DbSet < Award > Awards {
+            get;
+            set;
+        }
+        public DbSet < Genre > Genres {
+            get;
+            set;
+        }
+        public DbSet < Language > Languages {
+            get;
+            set;
+        }
+        public DbSet < Movie > Movies {
+            get;
+            set;
+        }
+        public DbSet < User > Users {
+            get;
+            set;
+        }
     }
 }
