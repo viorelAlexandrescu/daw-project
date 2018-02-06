325949d7fe3c20f35272755726c3b2540989205d
Viorel Alexandrescu <alex.viorel.117@gmail.com>
2018-02-06 02:50:18 +0200

    phase 2.1?

--- Startup.cs
+++ Startup.cs
@@ -11,32 +11,38 @@ using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using daw.Data;
 
-namespace daw
-{ 
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace daw {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration {
+            get;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var sqlConnectionString = Configuration.GetConnectionString("DataAccessMySqlProvider");
+        public void ConfigureServices(IServiceCollection services) {
+            var sqlConnectionString = Configuration.GetConnectionString("DefaultConnection");
 
-            services.AddDbContext<MyContext>(options =>   
-                options.UseMySQL(Configuration.GetConnectionString("DatabaseAccess")));
+            MySql.Data.MySqlClient.MySqlConnection conn;
+
+            try {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = sqlConnectionString;
+                conn.Open();
+            } catch (MySql.Data.MySqlClient.MySqlException ex) {
+                Console.WriteLine(ex.Message);
+            }
+
+            services.AddDbContext < MyContext > (options =>
+                options.UseMySQL(sqlConnectionString));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
